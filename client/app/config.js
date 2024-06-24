a73e9a460f5623ed821949a4acd8135454c03507
Mirazul3221 <mirazul3221@gmail.com>
2024-06-24 22:10:00 +0600

    U

--- client/app/config.js
+++ client/app/config.js
@@ -1,3 +1,3 @@
-// export const baseurl = 'https://yryyhdsjksbksdvgsidgashcjmv.vercel.app'
-export const baseurl = 'http://localhost:5050'
+export const baseurl = 'https://yryyhdsjksbksdvgsidgashcjmv.vercel.app'
+// export const baseurl = 'http://localhost:5050'
 //==========MyPass = @43gnertnr%&Yjk%(&(
