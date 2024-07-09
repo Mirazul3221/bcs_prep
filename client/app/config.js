c028e44eace03e0471d85b7c07fe627fed46bb25
Mirazul3221 <mirazul3221@gmail.com>
2024-07-09 20:50:49 +0600

    Change all system data

--- client/app/config.js
+++ client/app/config.js
@@ -1,4 +1,4 @@
 export let baseurl = 'https://yryyhdsjksbksdvgsidgashcjmv.vercel.app';
 //  baseurl = 'http://localhost:5050'
 //==========MyPass = @43gnertnr%&Yjk%(&(
-//====================//==================
+//====================//================
